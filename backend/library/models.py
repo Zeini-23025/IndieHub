5ef4c029ebf7c5e49cd7d8420a442d1bd3f4c7fd
zeini <23025@supnum.mr>
2025-12-16 22:47:39 +0000

    create libary models

--- backend/library/models.py
+++ backend/library/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from games.models import Game
+from users.models import User
 
-# Create your models here.
+
+class LibraryEntry(models.Model):
+    """
+    User's personal game library
+    """
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='library',
+        help_text='User'
+    )
+    game = models.ForeignKey(
+        Game,
+        on_delete=models.CASCADE,
+        related_name='in_libraries',
+        help_text='Game'
+    )
+    added_at = models.DateTimeField(
+        auto_now_add=True,
+        help_text='Date added to library'
+    )
+
+    class Meta:
+        verbose_name = 'Library Entry'
+        verbose_name_plural = 'Library Entries'
+        ordering = ['-added_at']
+        unique_together = ['user', 'game']
+
+    def __str__(self):
+        return f"{self.user.username}'s library: {self.game.title}"
