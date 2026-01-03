a333754a26ae8498c1701ca39fd2051f1c4bb19c
zeini <23025@supnum.mr>
2026-01-03 20:15:29 +0000

    chore(backend): update core settings and database

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -5,6 +5,9 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/welcome/', welcome),
@@ -13,6 +16,9 @@ urlpatterns = [
     path('api/library/', include('library.urls')),
     path('api/downloads/', include('downloads.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 schema_view = get_schema_view(
     openapi.Info(
