384aa5f31a08729fc349da65d2d154cfb38638dd
zeini <23025@supnum.mr>
2026-01-08 04:55:22 +0000

    test(api): implement welcome endpoint and public list validation

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -1,3 +1,10 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+class WelcomeTests(APITestCase):
+    def test_welcome_endpoint(self):
+        url = '/api/welcome/' # Manually defined in root urls.py
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['message'], "Welcome to the IndieHub API!")
