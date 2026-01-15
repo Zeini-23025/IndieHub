31ccf78d7519e74b05101573ae8ca9a3bf81afb4
zeini <23025@supnum.mr>
2026-01-15 23:06:58 +0000

    feat(desktop): implement main and preload processes

---
+++ desktop/electron/preload.ts
@@ -0,0 +1,20 @@
+import { contextBridge, ipcRenderer } from 'electron'
+import { electronAPI } from '@electron-toolkit/preload'
+
+// Custom APIs for renderer
+const api = {}
+
+// Use `contextBridge` APIs to expose IPC to the renderer
+if (process.contextIsolated) {
+    try {
+        contextBridge.exposeInMainWorld('electron', electronAPI)
+        contextBridge.exposeInMainWorld('api', api)
+    } catch (error) {
+        console.error(error)
+    }
+} else {
+    // @ts-ignore (define in dts)
+    window.electron = electronAPI
+    // @ts-ignore (define in dts)
+    window.api = api
+}
